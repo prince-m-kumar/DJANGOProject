8bdc07f9be8cc9dab1b62f829aa5a6b0de1f64fd
Prince kumar <prince@instachk.today>
2019-06-05 12:10:45 +0530

    Add: Model register in add admin

--- first_project/first_app/models.py
+++ first_project/first_app/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+class Topic(models.Model):
+    """docstring for Topic."""
+    top_name = models.CharField(max_length=264,unique=True)
+    # def __init__(self):
+    #     super(Topic, self).__init__()
+    #
+    #
+    def __str__(self):
+        return self.top_name
+
+
+class webPage(models.Model):
+    """docstring for webPage."""
+    topic = models.ForeignKey(Topic,on_delete=models.CASCADE)
+    name = models.CharField(max_length = 264,unique=True)
+    url= models.URLField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+class AccessRecord(models.Model):
+    name = models.ForeignKey(webPage,on_delete=models.CASCADE)
+    date = models.DateField()
+
+    """docstring for AccessRecord."""
+    def __str__(self):
+        return str(self.date)
+
+
+    # def __init__(self, arg):
+    #     super(webPage, self).__init__()
+    #     self.arg = arg
