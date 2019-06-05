9cd76b64589c54ce4f6d9460919a2879939ed1a4
Prince kumar <prince@instachk.today>
2019-06-05 14:19:14 +0530

    Add: faker implementation for generationg fack data

---
+++ first_project/populate_first_app.py
@@ -0,0 +1,36 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE','first_project.settings')
+
+import django
+django.setup()
+
+## Fake pop script
+import random
+from first_app.models import AccessRecord,webPage,Topic
+from faker import Faker
+fakegen = Faker()
+topic = ['Search','Socil','MarketPlace','News','Games']
+def add_topic():
+    t= Topic.objects.get_or_create(top_name=random.choice(topic))[0]
+    t.save()
+    return t
+def populate(N=5):
+
+    for entry in range(N):
+        #get the topic for the entry
+        top = add_topic()
+        # Create the fake data for entry
+        fake_url = fakegen.url()
+        fake_date = fakegen.date()
+        fake_name = fakegen.company()
+
+        #create new pweb page entry
+        webpg = webPage.objects.get_or_create(topic = top,url=fake_url,name = fake_name)[0]
+
+        # Create a fake access record for that web webPage
+        acc_rec = AccessRecord.objects.get_or_create(name = webpg,date = fake_date)[0]
+
+if __name__ == '__main__':
+    print("populating database")
+    populate(20)
+    print("populating complete")
